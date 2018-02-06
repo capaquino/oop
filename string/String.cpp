0a67fc7fa41eaaf965a1b4e60f88774a2b49d134
Nathan <nad46@zips.uakron.edu>
2018-02-06 11:38:29 -0500

    Fixed off-by-one error.

--- string/String.cpp
+++ string/String.cpp
@@ -1,15 +1,22 @@
 #include "String.hpp"
+// len is length of string not including null
+// str is full string and needs to include null
+// strlen does not count null
+// strcpy does copy null char
+// no, strncpy is not a typo
 
 String::String(const char* s)
 : _len(std::strlen(s)), _str(new char[_len+1])
 {
-    std::strncpy(_str, s, _len);
+    //std::strncpy(_str, s, _len+1);
+    std::strcpy(_str, s);
 }
 
 String::String(const String& s)
 : _len(s._len), _str(new char[_len+1])
 {
-    std::strncpy(_str, s._str, _len);
+    //std::strncpy(_str, s._str, _len+1);
+    std::strcpy(_str, s._str);
 }
 
 String& String::operator=(const char* s)
@@ -17,16 +24,18 @@ String& String::operator=(const char* s)
     delete[] _str;
     _len = std::strlen(s);
     _str = new char[_len+1];
-    std::strncpy(_str, s, _len);
+    //std::strncpy(_str, s, _len+1);
+    std::strcpy(_str, s);
     return *this;
 }
 
 String& String::operator=(const String &s)
 {
     delete[] _str;
-    _len = s._len+1; // or std::strlen(s._str);
+    _len = s._len; // or std::strlen(s._str);
     _str = new char[_len+1];
-    std::strncpy(_str, s._str, _len);
+    //std::strncpy(_str, s._str, _len+1);
+    std::strcpy(_str, s._str);
     return *this;
 }
 
--- string/main.cpp
+++ string/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <cassert>
+//#include <iostream>
 #include "String.hpp"
 
 int main()
@@ -8,24 +8,14 @@ int main()
 
     String s1 = "foo";
     String s2 = s1;
-    std::cout << "s1: " << s1 << std::endl;
-    std::cout << "s2: " << s2 << std::endl;
-    // my mind is literally being blown, why do i get malloc errors if I take
-    // these two out, or if I add additional copies of these statements.
-    // ** Ask in class **
     assert(s1 == s2);
 
     s2 = "cats";
-    //std::cout << "s1: " << s1 << std::endl;
-    //std::cout << "s2: " << s2 << std::endl;
     assert(s1 != s2);
     assert(s1 > s2);
     assert(s2 < s1);
 
-
     s2 = s1;
-    //std::cout << "s1: " << s1 << std::endl;
-    //std::cout << "s2: " << s2 << std::endl;
     assert(s1 >= s2);
     assert(s1 <= s2);
 
